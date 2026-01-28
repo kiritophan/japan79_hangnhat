cfc8ba644545ff8719cb0b69b076417856b3aef1
daupv-innove <admin@dck-tech.com>
2026-01-28 14:54:01 +0700

    re run

--- .vercel/output/static/chunks/b203_D3ehtKGt.mjs
+++
@@ -1,9 +0,0 @@
-async function getMod() {
-						return import('./b203_DPa137JJ.mjs');
-					}
-					const collectedLinks = [];
-					const collectedStyles = [];
-					const collectedScripts = [];
-					const defaultMod = { __astroPropagation: true, getMod, collectedLinks, collectedStyles, collectedScripts };
-
-export { defaultMod as default };
